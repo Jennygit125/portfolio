4dd7f92b8bbda4321a49e5518215c7d58082008a
Bakre Eniola Dahud <bakreeniola2@gmail.com>
2026-06-25 04:02:41 -0700

    Update Footer.jsx

--- my-portfolio/src/components/Footer.jsx
+++ my-portfolio/src/components/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     return(<footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pr-8 flex flex-wrap justify-between">
         {" "}
         <p className="text-sm text-muted-foreground">&copy; {new Date().getFullYear()}
-            thrill.io, All rights reserved
+             thrill.io, All rights reserved
         </p>
         <a href="#hero" className="p-2 rounded-full bg-primary/10 hover:bg-primary/20 text-primary transition-colors">
             <ArrowUp/>
